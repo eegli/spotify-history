22e3bc8c3a701d4812ab1a40d99a2ca0492d322f
Eric <43848365+eegli@users.noreply.github.com>
2021-03-28 14:19:24 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
 import Hello from './module/hello';
+import fetch from 'node-fetch';
 import { ScheduledHandler } from 'aws-lambda';
+import AWS from 'aws-sdk';
 
 export const handler: ScheduledHandler = async (
   event,
-  context
+  context,
+  callback
 ): Promise<void> => {
-  console.info(Hello + `, current env is: ${process.env.STAGE}`);
+  const s3 = new AWS.S3();
+  const bucketName = process.env.BUCKET_NAME || 'BUCKET_NAME';
+  // Call S3 to list the buckets
+  s3.listBuckets(function (err, data) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      console.log('Success', data.Buckets);
+    }
+  });
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+      const params = {
+        Key: `Test`,
+        Body: JSON.stringify(json),
+        Bucket: bucketName,
+        ContentType: 'application/json',
+      };
+      return s3.putObject(params).promise();
+    })
+    .then(() => {
+      console.log('The backup has been done');
+      callback(null);
+    })
+    .catch(error => {
+      console.log('Error: ', error);
+      callback(new Error(error));
+    });
+
+  /*   console.info(Hello + `, current env is: ${process.env.STAGE}`);
   console.warn(`Available in both prod and stg: ${process.env.PUBLIC_TEST}`);
   console.log('EVENT' + JSON.stringify(event, null, 2));
-  console.log('CONTEXT' + JSON.stringify(context, null, 2));
+  console.log('CONTEXT' + JSON.stringify(context, null, 2)); */
 };
